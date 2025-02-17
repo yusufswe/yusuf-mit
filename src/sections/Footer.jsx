0fb539618bb9e83ae64cc33af51e56bb29128d77
yusufswe <muhdysf89@gmail.com>
2025-02-17 11:28:43 +0700

    fix: sizing text footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="container mx-auto bg-black text-white/60 p-4 text-center text-sm">
+    <footer className="container mx-auto bg-black text-white font-semibold text-lg p-4 text-center ">
       <p>Yusuf © 2025</p>
     </footer>
   );
